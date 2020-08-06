0fe1d8d407fe8836ba02777c0a8580958e42bd6c
EnjoySoftware <47489953+enjoysoftware@users.noreply.github.com>
2020-08-06 20:54:42 +0900

    HTTP_X_FORWARDED_FORに変更、gethostbyaddrを追加

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 require_once 'vendor/autoload.php';
-
 $environment = [
-    "ip" => $_SERVER['REMOTE_ADDR'],
-    "gateway_name" => $_SERVER['REMOTE_HOST'],
+    "ip" => $_SERVER['HTTP_X_FORWARDED_FOR'],
+    "gateway_name" => gethostbyaddr($_SERVER['HTTP_X_FORWARDED_FOR']),
     "request_method" => $_SERVER['REQUEST_METHOD']
 ];
 echo json_encode($environment);
