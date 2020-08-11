42422bf2b58d1476e8880bb73c16ec1fcef67406
EnjoySoftware <47489953+enjoysoftware@users.noreply.github.com>
2020-08-11 12:53:09 +0900

    IPv6判定プログラムを作成、エラー表示も作成

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 header('Access-Control-Allow-Origin: https://open-kakuninkun.github.io');
 require_once 'vendor/autoload.php';
+function checkIpFormat($ip) {
+    if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)){
+       return "IPv4";
+    }
+    else{
+       if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)){
+       return "IPv6";
+       }else{return "不明";}
+    }
+}
 if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
     $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
     if(strpos($ipaddress, ',') != false) {
@@ -10,9 +20,13 @@ if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
 } else {
     $ipaddress = $_SERVER['REMOTE_ADDR'];
 }
+if(!empty($ipaddress)){
+    $ip_format = checkIpFormat($ipaddress);
+}
 $environment = [
-    "ip" => $ipaddress,
-    "gateway_name" => gethostbyaddr($ipaddress)
+    "ip" => $ipaddress != null ? $ipaddress : "不明",
+    "gateway_name" => gethostbyaddr($ipaddress) != false ? gethostbyaddr($ipaddress) : "不明", 
+    "ip_format" => $ip_format != null ? $ip_format : "不明"
 ];
 echo json_encode($environment);
 ?>
